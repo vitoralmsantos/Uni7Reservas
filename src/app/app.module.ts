e44d6b64b1d16d4a888de2be8942dcb6264856c4
vitoralmsantos <vitoralm@gmail.com>
2018-08-30 10:57:38 -0300

    NIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,9 @@ import { UsuarioComponent } from './admin/usuario.component';
 import { LocalComponent } from './admin/local.component';
 import { EquipamentoComponent } from './admin/equipamento.component';
 import { AdminComponent } from './admin/admin.component';
-import { RelatorioReservasComponent } from './relatorio-reservas.component';
-import { RelatorioAvaliacaoComponent } from './relatorio-avaliacao.component';
+import { RelatorioReservasComponent } from './relatorios/relatorio-reservas.component';
+import { RelatorioAvaliacaoComponent } from './relatorios/relatorio-avaliacao.component';
+import { RelatoriosComponent } from './relatorios/relatorios.component';
 
 const appRoutes: Routes = [  
   { path: '', component: HomeComponent},
@@ -21,6 +22,12 @@ const appRoutes: Routes = [
         {path : '', component: ReservaComponent},
         {path : 'reserva', component: ReservaComponent},
         {path : 'perfil', component: PerfilComponent},
+        {path : 'relatorios', component: RelatoriosComponent,
+          children:[
+            {path : 'reservas', component: RelatorioReservasComponent},
+            {path : 'avaliacao', component: RelatorioAvaliacaoComponent}
+          ]
+        },
         {path : 'admin', component: AdminComponent,
           children:[
             {path : 'usuario', component: UsuarioComponent},
@@ -44,7 +51,8 @@ const appRoutes: Routes = [
     EquipamentoComponent,
     AdminComponent,
     RelatorioReservasComponent,
-    RelatorioAvaliacaoComponent
+    RelatorioAvaliacaoComponent,
+    RelatoriosComponent
   ],
   imports: [
     RouterModule.forRoot(
---
+++ src/app/relatorios/relatorios.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'uni7res-relatorios',
+  templateUrl: './relatorios.component.html',
+  styleUrls: ['./relatorios.component.css']
+})
+export class RelatoriosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
