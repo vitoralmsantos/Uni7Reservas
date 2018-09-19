fdfe0d5e9b9c7d5a810d597eb42b49c74da849ad
vitoralmsantos <vitoralm@gmail.com>
2018-09-19 10:16:20 -0300

    NIP-VITOR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { RootComponent } from './root.component';
-import { HomeComponent } from './home.component';
-import { PrincipalComponent } from './principal.component';
-import { ReservaComponent } from './reserva.component';
-import { PerfilComponent } from './perfil.component';
-import { UsuarioComponent } from './admin/usuario.component';
-import { LocalComponent } from './admin/local.component';
-import { EquipamentoComponent } from './admin/equipamento.component';
-import { AdminComponent } from './admin/admin.component';
-import { RelatorioReservasComponent } from './relatorios/relatorio-reservas.component';
-import { RelatorioAvaliacaoComponent } from './relatorios/relatorio-avaliacao.component';
-import { RelatoriosComponent } from './relatorios/relatorios.component';
-import { AvaliacaoComponent } from './admin/avaliacao.component';
-import { CategoriaComponent } from './admin/categoria.component';
+import { RootComponent } from './view/root.component';
+import { HomeComponent } from './view/home.component';
+import { PrincipalComponent } from './view/principal.component';
+import { ReservaComponent } from './view/reserva.component';
+import { PerfilComponent } from './view/perfil.component';
+import { UsuarioComponent } from './view/admin/usuario.component';
+import { LocalComponent } from './view/admin/local.component';
+import { EquipamentoComponent } from './view/admin/equipamento.component';
+import { AdminComponent } from './view/admin/admin.component';
+import { RelatorioReservasComponent } from './view/relatorios/relatorio-reservas.component';
+import { RelatorioAvaliacaoComponent } from './view/relatorios/relatorio-avaliacao.component';
+import { RelatoriosComponent } from './view/relatorios/relatorios.component';
+import { AvaliacaoComponent } from './view/admin/avaliacao.component';
+import { CategoriaComponent } from './view/admin/categoria.component';
 
 const appRoutes: Routes = [  
   { path: '', component: HomeComponent},
