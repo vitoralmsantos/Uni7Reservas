ff7d510f77c0d6944b496d7175b45a99b1f6346a
igorsousa0 <31631156+igorsousa0@users.noreply.github.com>
2018-09-25 17:37:46 -0300

    NIP-Igor

--- src/app/view/admin/local.component.ts
+++ src/app/view/admin/local.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Local } from '../../model/local.model';
+import { LocalService } from '../../services/local.service';
+declare var jquery:any;
+declare var $ :any;
 
 @Component({
   selector: 'uni7res-local',
@@ -6,10 +10,103 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./local.component.css']
 })
 export class LocalComponent implements OnInit {
+  locais: Local[];
+  local: Local;
+  erroDetalhe: string;
+  selectedIndex: number;
 
-  constructor() { }
+
+  constructor(private localService: LocalService) { }
 
   ngOnInit() {
+    this.getLocais();
+    this.limpar();
+  }
+ 
+  getLocais(): void {
+    this.localService.getLocais()
+      .subscribe(response => {
+        if (response.Status == 0) {
+          this.locais = response.Locais
+        }
+        else {
+          this.mostraErro(response.Detalhes)
+        }
+      });
+  }
+
+  getLocal(id): void {
+    this.localService.getLocal(id)
+      .subscribe(response => { this.local = response.Local });
+  }
+
+
+  limpar(): void {
+    this.local = new Local()
+  }
+
+  inserir(): void {
+    this.localService.addLocal(this.local)
+      .subscribe(response => {
+        if (response.Status == 0) {
+          this.getLocais();
+        }
+        else {
+          this.mostraErro(response.Detalhes)
+        }
+      });
+  }
+
+  atualizar(): void {
+    this.localService.updateLocal(this.local)
+      .subscribe(response => {
+        if (response.Status == 0) {
+          this.locais[this.selectedIndex].Nome = response.Local.Nome;
+          this.locais[this.selectedIndex].Reservavel = response.Local.Reservavel;
+          this.locais[this.selectedIndex].Disponivel = response.Local.Disponivel;
+          this.locais[this.selectedIndex].Tipo = response.Local.Tipo;
+
+        }
+        else {
+          this.mostraErro(response.Detalhes)
+        }
+      });
+  }
+
+  remover(id): void {
+    this.localService.deleteLocal(id)
+      .subscribe(response => {
+        if (response.Status == 0) {
+          this.getLocais();
+        }
+        else {
+          this.mostraErro(response.Detalhes)
+        }
+      });
+  }
+
+  carregar(index): void {
+    this.selectedIndex = index
+    this.local.Id = this.locais[index].Id
+    this.local.Nome = this.locais[index].Nome
+    this.local.Reservavel = this.locais[index].Reservavel
+    this.local.Disponivel = this.locais[index].Disponivel
+    this.local.Tipo = this.locais[index].Tipo
+
+  }
+
+  registrar(): void {
+    if (this.local.Id === undefined) {
+      this.inserir()
+    }
+    else {
+      this.atualizar()
+    }
+  }
+
+  mostraErro(detalhe): void {
+    this.erroDetalhe = detalhe
+    $('#modalErro').modal('show')
   }
 
 }
