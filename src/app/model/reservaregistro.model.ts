9c09b8e35a858b832619ae7b4fd961e2d1d79428
vitoralmsantos <vitoralm@gmail.com>
2018-10-01 15:39:17 -0300

    NIP-Vitor

---
+++ src/app/model/reservaregistro.model.ts
@@ -0,0 +1,12 @@
+export class ReservaRegistro {
+    Id: number;
+    Data: string;
+    Horario: string;
+    ReservadoEm: string;
+    Turno : string;
+    Obs: string;
+    IdLocal: number;
+    IdUsuario: number;
+    IdEquipamento1: number;
+    IdEquipamento2: number;
+}
--- src/app/view/reserva.component.ts
+++ src/app/view/reserva.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Reserva } from '../model/reserva.model';
+import { ReservaRegistro } from '../model/reservaregistro.model';
+import { Local } from '../model/local.model';
+import { Equipamento } from '../model/software.model';
 
 @Component({
   selector: 'uni7res-reserva',
@@ -6,10 +10,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reserva.component.css']
 })
 export class ReservaComponent implements OnInit {
+  reserva: ReservaRegistro;
+  horario: number;
+  somenteLabs : boolean;
+  reservas: Reserva[];
+  locais: Local[];
+  equipamentos1: Equipamento[];
+  equipamentos2: Equipamento[];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  iniciarReserva(): void {
+    switch (this.horario) {
+      case 1:
+        this.reserva.Turno = 'M';
+        this.reserva.Horario = 'AB';
+        break;
+      case 2:
+        this.reserva.Turno = 'M';
+        this.reserva.Horario = 'CD';
+        break;
+      case 3:
+        this.reserva.Turno = 'M';
+        this.reserva.Horario = 'EF';
+        break;
+      case 4:
+        this.reserva.Turno = 'T';
+        this.reserva.Horario = 'AB';
+        break;
+      case 5:
+        this.reserva.Turno = 'T';
+        this.reserva.Horario = 'CD';
+        break;
+      case 6:
+        this.reserva.Turno = 'T';
+        this.reserva.Horario = 'EF';
+        break;
+      case 7:
+        this.reserva.Turno = 'N';
+        this.reserva.Horario = 'AB';
+        break;
+      case 8:
+        this.reserva.Turno = 'N';
+        this.reserva.Horario = 'CD';
+        break;
+    }
   }
 
 }
