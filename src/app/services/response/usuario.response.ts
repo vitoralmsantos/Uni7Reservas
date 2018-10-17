3bd217d6682a28d3e41d6398e0a8206d2f4980c9
vitoralmsantos <vitoralm@gmail.com>
2018-10-17 16:12:21 -0300

    NIP-Vitor

--- src/app/services/response/usuario.response.ts
+++
@@ -1,6 +0,0 @@
-import { BaseResponse } from './base.response'
-import { Usuario } from '../../model/usuario.model'
-
-export class UsuarioResponse extends BaseResponse {
-	Usuario : Usuario;
-}
--- src/app/services/response/usuarios.response.ts
+++
@@ -1,6 +0,0 @@
-import { BaseResponse } from './base.response'
-import { Usuario } from '../../model/usuario.model'
-
-export class UsuariosResponse extends BaseResponse {
-	Usuarios : Usuario[];
-}
